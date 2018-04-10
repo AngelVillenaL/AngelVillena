08fd83badd56e5f0c12be1d78d19a9251c6abea1
AngelVillena <angeldvillenal1342001@gmial.com>
2018-04-10 18:18:24 -0500

    hgf

---
+++ AngelVillena/Assets/MovementTest/Movimiento.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+	public float speed;
+	public Vector3 direction;
+	public Vector3 limit;
+	public Rigidbody miRigidBody;
+	public Vector3 posicionInicial;
+
+	// Use this for initialization
+	void Start () {
+		miRigidBody = GetComponent<Rigidbody> ();
+		posicionInicial = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.A) && transform.position.x > -limit.x) {
+			transform.Translate (Vector3.left * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.D) && transform.position.x < limit.x) {
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.W) && transform.position.z < limit.z) {
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.S) && transform.position.z > -limit.z) {
+			transform.Translate (Vector3.back * speed * Time.deltaTime);
+		}
+
+	}
+
+	void OnCollisionEnter (Collision col) 
+	{
+		if (col.gameObject.CompareTag("Wall"))
+		{
+			miRigidBody.MovePosition (posicionInicial);
+			Debug.Log ("awake");
+		}
+		else if (col.gameObject.CompareTag("Exit"))
+		{
+			Debug.Log ("Felicidades! Ganaste!");
+		}
+
+	}
+
+	//Debug.Log ("Intentalo de nuevo...");
+	//miRigidBody.MovePosition (posicionInicial);
+
+
+	}
