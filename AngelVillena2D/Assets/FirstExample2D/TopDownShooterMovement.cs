807620e5c71e916d58e6e2c4c5f48757b31acba0
AngelVillenaL <angeldvillenal1342001@gmail.com>
2018-04-24 17:54:24 -0500

    hjkh

--- AngelVillena2D/Assets/FirstExample2D/TopDownShooterMovement.cs
+++ AngelVillena2D/Assets/FirstExample2D/TopDownShooterMovement.cs
@@ -10,7 +10,7 @@ public class TopDownShooterMovement : MonoBehaviour {
     public GameObject bullet;
 
     public List<Color> colors = new List<Color> ();
-    int ColorIndex = 0;
+    int colorIndex = 0;
 
     public SpriteRenderer spriteRenderer;
 
@@ -21,56 +21,63 @@ public class TopDownShooterMovement : MonoBehaviour {
 
         public Axis (string _name, KeyCode _negative, KeyCode _positive) {
             name = _name;
-            positive = _positive;
             negative = _negative;
+            positive = _positive;
         }
-
     }
 
     List<Axis> axisList = new List<Axis> ();
 
 	// Use this for initialization
 	void Start () {
+        spriteRenderer.color = colors[colorIndex];
         axisList.Add (new Axis ("Horizontal", KeyCode.A, KeyCode.D));
-        axisList.Add (new Axis ("Horizontal", KeyCode.S, KeyCode.W));
+        axisList.Add (new Axis ("Vertical", KeyCode.S, KeyCode.W));
         axisList.Add (new Axis ("Arrow_H", KeyCode.LeftArrow, KeyCode.RightArrow));
-
-    }
+	}
 	
 	// Update is called once per frame
 	void Update () {
 
         transform.Translate (Vector3.right * GetAxis ("Horizontal") * speed * Time.deltaTime, Space.World);
         transform.Translate (Vector3.up * GetAxis ("Vertical") * speed * Time.deltaTime, Space.World);
-        transform.Rotate (Vector3.back * GetAxis("Arrow_H") * angularVelocity * Time.deltaTime);
+        transform.Rotate (Vector3.back * GetAxis ("Arrow_H") * angularVelocity * Time.deltaTime);
 
+        if (Input.GetKeyDown (KeyCode.E)) {
+            MoveColor ();
+        }
 
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            Shoot ();
+        }
+	}
+
+    void Shoot () {
+        SpriteRenderer tempRenderer = Instantiate (bullet, transform.Find ("Cannon").position, transform.rotation).GetComponent<SpriteRenderer>();
+        tempRenderer.color = spriteRenderer.color;
+        Destroy (tempRenderer.gameObject, 2);
+    }
+
+    void MoveColor () {
+        colorIndex = (colorIndex >= colors.Count - 1) ? 0 : colorIndex + 1;
+        spriteRenderer.color = colors[colorIndex];
     }
 
     int GetAxis (string axisName) {
-
         Axis axis = axisList.Find (target => target.name == axisName);
         int axisValue = 0;
         if (Input.GetKey (axis.negative)) {
             axisValue += -1;
         }
         if (Input.GetKey (axis.positive)) {
-            axisValue += +1;
+            axisValue += 1;
         }
         return axisValue;
-
     }
 
-    void Shoot ()
-    {
-        SpriteRenderer spriteRenderer = (Instantiate(bullet, transform.Find ("Cannon").position, transform.rotation).GetComponent<SpriteRenderer>);
-        tempRenderer.color = spriteRenderer.color;
-        Destroy (tempRenderer.gameObject, 2); 
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.CompareTag ("Block")) {
+            Debug.Log ("Block collision!");
+        }
     }
-    void MoveColor ()
-    {
-        ColorIndex = (ColorIndex >= colors.Count - 1) ? 0 : ColorIndex + 1;
-        SpriteRenderer.color = colors[ColorIndex];
-    }
-
 }
