44e043d0a0084ba0ceb4cec94d8b81bad1f59ef4
AngelVillenaL <angeldvillenal1342001@gmail.com>
2018-05-24 18:38:36 -0500

    xjf

--- AngelVillena2D/Assets/FirstExample2D/Scripts/ChangeSprite.cs
+++ AngelVillena2D/Assets/FirstExample2D/Scripts/ChangeSprite.cs
@@ -1,57 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ChangeSprite : MonoBehaviour {
 
-private SpriteRenderer spriteRenderer;
-public Sprite spriteUp;
-public Sprite spriteDown;
-public Sprite spriteRight;
-public Sprite spriteLeft;
-
-public Animator animator;
-public RuntimeAnimatorController WalkUp;
-public RuntimeAnimatorController WalkDown;
-public RuntimeAnimatorController WalkRight;
-public RuntimeAnimatorController WalkLeft;
-
-int directionIndex = 0;
-
+    private SpriteRenderer spriteRenderer;
+    public Sprite spriteUp;
+    public Sprite spriteDown;
+    public Sprite spriteLeft;
+    public Sprite spriteRight;
 
 	// Use this for initialization
 	void Start () {
-		spriteRenderer = GetComponent<SpriteRenderer>();
-		spriteRenderer.sprite = spriteUp;
-		animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = spriteUp;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.A)) {
-			spriteRenderer.sprite = spriteLeft;
-			animator = GetComponent<Animator>();
-			}
+		
+        if (Input.GetKeyDown(KeyCode.A)) {
+            spriteRenderer.sprite = spriteLeft;
+        }
 
-		if (Input.GetKeyDown(KeyCode.D)) {
-			spriteRenderer.sprite = spriteRight;
-			}
+        if (Input.GetKeyDown(KeyCode.D)) {
+            spriteRenderer.sprite = spriteRight;
+        }
 
-		if (Input.GetKeyDown(KeyCode.W)) {
-			spriteRenderer.sprite = spriteUp;
-			}
+        if (Input.GetKeyDown(KeyCode.W)) {
+            spriteRenderer.sprite = spriteUp;
+        }
 
-		if (Input.GetKeyDown(KeyCode.S)) {
-			spriteRenderer.sprite = spriteDown;
-			}
-	
+        if (Input.GetKeyDown(KeyCode.S)) {
+            spriteRenderer.sprite = spriteDown;
+        }
 
-			animator.SetInteger("direction", directionIndex);
-
-		if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) {
-				animator.SetInteger("direction", -1);
-			} 
-
-	} 
+	}
 }
