53e58a8026fbc2e6a3de65c8297868d90a63be33
AngelVillenaL <angeldvillenal1342001@gmail.com>
2018-07-05 22:05:09 -0500

    fsdv

---
+++ 3D/3D/Assets/FirstTest3D/Scripts/Enemies/BasicEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : EnemyObject {
+
+	public int health;
+	bool invulnerable = false;
+	public platformMovement3D target;
+	public Vector3 planarTargetDistance { get { return new Vector3 (target.transform.position.x, transform.position.y, target.transform.position.z)}}
+	public float colorIndex = 0f;
+
+
+	void Update () {
+		if (target != null) {
+			transform.forward = (planarTargetDistance - transform.position).normalized;
+		}
+		GetComponent<Renderer>().material.color = damageGradient.Evaluate;
+	}
+
+	public override void TakeDamage () {
+		if (!invulnerable) { 
+		health--;
+		GetComponent<Animator>().SetTrigger("TakeDamage");
+		invulnerable = true;
+		}
+	}
+	
+	public void ResetInvulnerable () {
+		invulnerable = false;
+	}
+}
