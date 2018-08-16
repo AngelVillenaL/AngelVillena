39f49c3aadb6c0c280c5b64148d9e3f95e10b9ee
AngelVillenaL <angeldvillenal1342001@gmail.com>
2018-08-16 18:28:07 -0500

    iyh

---
+++ FP(Test) Inv/Assets/RPG_BOX_Free/Scripts/ScriptablObjects/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Item : ScriptableObject
+{
+    public Sprite itemIcon;
+    public string itemName;
+    public int SellPrice;
+
+}
+
+[System.Serializable]
+public class StatValue
+{
+    public MainStat TheStat;
+    public int TheValue;
+
+    public StatValue(MainStat MS, int Value)
+    {
+        TheStat = MS;
+        TheValue = Value;
+    }
+}
